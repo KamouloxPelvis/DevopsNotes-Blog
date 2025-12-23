b583e4d1e336ce5ee613d37f549b6d5415188f6e
Kamal Guidadou <kamal.guidadou@gmail.com>
2025-12-23 21:48:09 +0100

    Modified src/api/articles.ts in order to fix fetch issues online

--- frontend/src/api/articles.ts
+++ frontend/src/api/articles.ts
@@ -1,7 +1,8 @@
 // src/api/articles.ts
 import { Article } from '../types/articles';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL ?? 'http://localhost:5000/api';
+
 
 export interface CreateArticlePayload {
   title: string;
