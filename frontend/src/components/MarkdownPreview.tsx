a505e7fcb6eeab316b003ef0481c5826fe59ce3d
Kamal Guidadou <kamal.guidadou@gmail.com>
2026-01-03 16:32:10 +0100

    Modified UX/UI of articles and creation of articles | modified texttoolbar

--- frontend/src/components/MarkdownPreview.tsx
+++ frontend/src/components/MarkdownPreview.tsx
@@ -1,31 +1,38 @@
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'; // Style proche de VS Code
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-export default function MarkdownPreview({ content }: { content: string }) {
+interface MarkdownPreviewProps {
+  content: string;
+  className?: string;
+}
+
+export default function MarkdownPreview({ content, className }: MarkdownPreviewProps) {
   return (
-    <ReactMarkdown
-      components={{
-        code({ node, inline, className, children, ...props }: any) {
-          const match = /language-(\w+)/.exec(className || '');
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={vscDarkPlus}
-              language={match[1]}
-              PreTag="div"
-              {...props}
-            >
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {content}
-    </ReactMarkdown>
+    <div className={className}>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={vscDarkPlus}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
   );
 }
